11aad6c96c3aa5e06527118c0695f9f7f56dabcb
Sarah21425 <sara.asar321@gmail.com>
2025-07-06 12:14:54 +0300

    Update 0001_initial.py

--- Task2/migrations/0001_initial.py
+++ Task2/migrations/0001_initial.py
@@ -16,6 +16,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('username', models.CharField(max_length=150, unique=True)),
+                ('firstname', models.CharField(max_length=150, unique=True)),
+                ('lastname', models.CharField(max_length=150, unique=True)),
                 ('email', models.EmailField(blank=True, max_length=254, null=True, unique=True)),
                 ('password', models.CharField(max_length=128)),
                 ('submitted', models.BooleanField(default=False)),
