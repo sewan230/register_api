d822cf597a18e33bfd0628057e40ef8007775152
Sarah21425 <sara.asar321@gmail.com>
2025-07-06 11:11:23 +0300

    Update models.py by Sarah
    add firstname and lastname fields to use model

--- Task2/models.py
+++ Task2/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Task2(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
     username = models.CharField(max_length=150)
     email = models.EmailField(unique=True, blank=True, null=True)
     password = models.CharField(max_length=128)  # You can hash this later
