b371819ecbf70e6955bda60a7ee871b54dd1c959
Sarah21425 <sara.asar321@gmail.com>
2025-07-06 12:10:40 +0300

    Update models.py
    Add firstname and lastname fields to user models

--- Task2/models.py
+++ Task2/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Task2(models.Model):
     username = models.CharField(max_length=150)
+    firstname = models.CharField(max_length=150)
+    lastname = models.CharField(max_length=150)
     email = models.EmailField(unique=True, blank=True, null=True)
     password = models.CharField(max_length=128)  # You can hash this later
     submitted = models.BooleanField(default=False)
